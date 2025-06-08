4adb993026e0870d7663e0dc938f6dafbc985060
Itzo <ja.sam.ico@gmail.com>
2025-06-08 22:13:25 +0200

    Updated MainActivityUITest.kt test file

--- app/src/androidTest/java/com/example/mtglifetracker/MainActivityUITest.kt
+++ app/src/androidTest/java/com/example/mtglifetracker/MainActivityUITest.kt
@@ -1,36 +1,117 @@
 package com.example.mtglifetracker
 
+import android.view.InputDevice
+import android.view.MotionEvent
+import android.view.View
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.GeneralClickAction
+import androidx.test.espresso.action.Press
+import androidx.test.espresso.action.Tap
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.BoundedMatcher
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.mtglifetracker.view.LifeCounterView
+import com.example.mtglifetracker.view.RotatableLayout
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.ArrayList
 
-/**
- * UI tests for the MainActivity. This class uses Hilt for dependency injection
- * and Espresso for interacting with and verifying UI components.
- */
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
 class MainActivityUITest {
 
-    // HiltRule manages the Hilt components' state and is used to inject dependencies into the test.
     @get:Rule(order = 0)
     val hiltRule = HiltAndroidRule(this)
 
-    // ActivityScenarioRule launches the activity under test before each test method.
     @get:Rule(order = 1)
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun settingsIcon_shouldBeDisplayed_onLaunch() {
-        // Verifies that the settings icon is visible when the activity starts.
         onView(withId(R.id.settingsIcon)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun lifeTotal_shouldChange_andDeltaShouldAppear_whenPlayerSegmentIsClicked() {
+        val lifeCounterMatcher = allOf(
+            withId(R.id.lifeCounter),
+            isDescendantOfA(withAngle(0))
+        )
+        val deltaCounterMatcher = allOf(
+            withId(R.id.deltaCounter),
+            isDescendantOfA(withAngle(0))
+        )
+
+        onView(lifeCounterMatcher).perform(clickInXPercent(25))
+        onView(lifeCounterMatcher).check(matches(withText("39")))
+        onView(deltaCounterMatcher).check(matches(allOf(isDisplayed(), withText("-1"))))
+
+        onView(lifeCounterMatcher).perform(clickInXPercent(75))
+        onView(lifeCounterMatcher).check(matches(withText("40")))
+        onView(deltaCounterMatcher).check(matches(allOf(isDisplayed(), withText("0"))))
+    }
+
+    @Test
+    fun playerLayout_shouldUpdate_whenPlayerCountIsChangedViaSettings() {
+        onView(withId(R.id.settingsIcon)).perform(click())
+        onView(withText("Number of Players")).perform(click())
+        onView(withText("4")).perform(click())
+
+        onView(isRoot()).check(matches(hasViewCount(LifeCounterView::class.java, 4)))
+    }
+
+    private fun clickInXPercent(pct: Int): ViewAction {
+        return GeneralClickAction(
+            Tap.SINGLE,
+            { view ->
+                val screenPos = IntArray(2)
+                view.getLocationOnScreen(screenPos)
+                val screenX = screenPos[0] + (view.width * pct / 100f)
+                val screenY = screenPos[1] + (view.height / 2f)
+                floatArrayOf(screenX, screenY)
+            },
+            Press.FINGER,
+            InputDevice.SOURCE_UNKNOWN,
+            MotionEvent.BUTTON_PRIMARY
+        )
+    }
+
+    private fun hasViewCount(viewClass: Class<out View>, expectedCount: Int): Matcher<View> {
+        return object : org.hamcrest.BaseMatcher<View>() {
+            override fun describeTo(description: Description?) {
+                description?.appendText("has $expectedCount views of class ${viewClass.simpleName}")
+            }
+
+            override fun matches(item: Any?): Boolean {
+                val rootView = item as? View ?: return false
+                val views = ArrayList<View>()
+                rootView.findViewsWithText(views, "40", View.FIND_VIEWS_WITH_TEXT)
+                return views.filterIsInstance(viewClass).size == expectedCount
+            }
+        }
+    }
+
+    private fun withAngle(expectedAngle: Int): Matcher<View> {
+        return object : BoundedMatcher<View, RotatableLayout>(RotatableLayout::class.java) {
+            // FIX: Redundant qualifier removed
+            override fun describeTo(description: Description) {
+                description.appendText("with angle: $expectedAngle")
+            }
+
+            override fun matchesSafely(item: RotatableLayout): Boolean {
+                return item.angle == expectedAngle
+            }
+        }
     }
 }
